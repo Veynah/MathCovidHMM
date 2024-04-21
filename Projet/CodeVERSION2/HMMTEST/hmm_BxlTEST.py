713768e96363f0959e7a0ecca964da16f5e9ba17
Adam Tangiev <tangiev96@gmail.com>
2024-04-21 17:39:10 +0200

    changements niveaux df filtered
    maintenant les données utilisées par les HMM utilisent des chiffres à 2 après la virgule

--- Projet/CodeVERSION2/HMMTEST/hmm_BxlTEST.py
+++ Projet/CodeVERSION2/HMMTEST/hmm_BxlTEST.py
@@ -5,19 +5,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Charger les données
-df = pd.read_csv("../Filtered_Covid19_data.csv")
+df = pd.read_csv("../Filtered_Covid19_data.csv")  # Assurez-vous que le chemin d'accès est correct
 df["DATE"] = pd.to_datetime(df["DATE"])
 df_bruxelles = df[df["TX_DESCR_FR"] == "Bruxelles"]  # Filtrer pour Bruxelles
 
 # Préparer les données
 X = df_bruxelles['CASES_PER_10K_MA'].values.reshape(-1, 1)
 
-# Diviser les données
-X_train, X_test = train_test_split(X, test_size=0.5, random_state=42, shuffle=False)
-
+# Diviser les données en 70% train et 30% test
+X_train, X_test, dates_train, dates_test = train_test_split(X, df_bruxelles['DATE'], test_size=0.3, random_state=425, shuffle=False)
 
 # Configuration du modèle HMM
-model = hmm.GaussianHMM(n_components=7, covariance_type="full", n_iter=10000)
+model = hmm.GaussianHMM(n_components=7, covariance_type="full", n_iter=10000, random_state=425)
 
 # Entraînement du modèle
 model.fit(X_train)
@@ -31,7 +30,7 @@ print(hidden_states_test)
 
 # Visualisation des états au fil du temps
 plt.figure(figsize=(15, 5))
-plt.plot(df_bruxelles['DATE'][len(X_train):], hidden_states_test, drawstyle="steps-post")
+plt.plot(dates_test.reset_index(drop=True), hidden_states_test, drawstyle="steps-post")
 plt.title('États cachés prédits sur les données de test')
 plt.xlabel('Date')
 plt.ylabel('État caché')
--- Projet/CodeVERSION2/df_filtered.py
+++ Projet/CodeVERSION2/df_filtered.py
@@ -34,11 +34,13 @@ def moving_average_filter(s, window_size=21):
 for commune in df["TX_DESCR_FR"].unique():
     mask = df["TX_DESCR_FR"] == commune
     df.loc[mask, "CASES_PER_10K"] = hampel_filter_for_series(
-        df.loc[mask, "CASES_PER_10K"], window_size=7, n_sigmas=3
+        df.loc[mask, "CASES_PER_10K"], window_size=21, n_sigmas=3
     )
     df.loc[mask, "CASES_PER_10K_MA"] = moving_average_filter(
         df.loc[mask, "CASES_PER_10K"]
     )
+    # Arrondissement des données
+    df.loc[mask, "CASES_PER_10K_MA"] = np.round(df.loc[mask, "CASES_PER_10K_MA"], 2)
 
 # Interpolation pour les valeurs NaN générées par le lissage moyen mobile
 df["CASES_PER_10K_MA"] = df.groupby("TX_DESCR_FR")["CASES_PER_10K_MA"].transform(lambda x: x.interpolate())
